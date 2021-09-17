a7839bcc6973410034f9e445bb2c9d74eedb6db4
Note <seeyou85@gmail.com>
2021-09-17 08:30:17 +0300

    добавлен текст

--- app/src/main/java/stas/batura/testdownload/MainActivity.kt
+++ app/src/main/java/stas/batura/testdownload/MainActivity.kt
@@ -9,12 +9,14 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
+import androidx.lifecycle.ViewModelProvider
 import stas.batura.testdownload.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
+    private lateinit var viewModel: MainViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,6 +25,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         setSupportActionBar(binding.toolbar)
+
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
 
         binding.fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
--- app/src/main/java/stas/batura/testdownload/MainViewModel.kt
+++ app/src/main/java/stas/batura/testdownload/MainViewModel.kt
@@ -2,9 +2,18 @@ package stas.batura.testdownload
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import java.lang.StringBuilder
 
 class MainViewModel: ViewModel() {
 
-    val textValue: MutableLiveData<String> = MutableLiveData("")
+    val textValue: MutableLiveData<StringBuilder> = MutableLiveData(StringBuilder(""))
+
+    fun addText(text: String) {
+        textValue.value?.append(text)?.append("\n")
+    }
+
+    fun clearText(text: String) {
+        textValue.value = StringBuilder("")
+    }
 
 }
